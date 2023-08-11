fc7010eb0fe45444ada149736d188c5260e4e32e
cdkw2 <67598073+cdkw2@users.noreply.github.com>
2023-08-11 22:44:51 +0530

    Added a new background and music because why nor?

--- full-website/script.js
+++ full-website/script.js
@@ -1,10 +1,30 @@
-function scroller() {
-  $(".smooth").on("scroll", function() {
-    let scrollPos = $(this).scrollTop();
-    $(".parallax").css({
-      top: scrollPos / 2 + "px",
-      opacity: 1 - scrollPos / 200
-    });
-  });
+var audio = new Audio('Nuvema-Town.mp3');
+var isAudioPlaying = false;
+
+// Set the desired volume level (between 0 and 1)
+audio.volume = 0.5; // Adjust this value to your preference
+
+function playAudio() {
+  if (!isAudioPlaying) {
+    audio.play();
+    isAudioPlaying = true;
+  }
 }
-scroller();
+
+$(document).ready(function() {
+  // Play audio when the page is clicked
+  $(document).on("click", function() {
+    playAudio();
+  });
+
+  function scroller() {
+    $(".smooth").on("scroll", function() {
+      let scrollPos = $(this).scrollTop();
+      $(".parallax").css({
+        top: scrollPos / 2 + "px",
+        opacity: 1 - scrollPos / 200
+      });
+    });
+  }
+  scroller();
+});
